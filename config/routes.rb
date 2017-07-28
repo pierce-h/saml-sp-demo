c549789af11923ce8a799f5ea98d5c492a132cb6
Pierce <pierceh.dev@gmail.com>
2017-07-28 07:42:57 -0500

    W.I.P.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :saml, only: :index
+  # resources :saml, only: :index
 
-  get "saml/sso",      to: "saml#sso",      as: :saml_sso
+  # get "saml/sso",      to: "saml#sso",      as: :saml_sso
   get "saml/acs",      to: "saml#acs",      as: :saml_acs
   get "saml/metadata", to: "saml#metadata", as: :saml_metadata
-  get "saml/logout",   to: "saml#logout",   as: :saml_logout
+  # get "saml/logout",   to: "saml#logout",   as: :saml_logout
 
   root to: "pages#home"
 end
